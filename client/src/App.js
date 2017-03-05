ec806aeaf3d285842b2b5474c567b1ae20241831
CY <onions.yang@gmail.com>
2017-03-05 16:01:27 +0800

    create server lists

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
- 
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import AppBar from 'material-ui/AppBar'
+import ServerList from './ServerList.js'
+
+const styles = {
+  width: '60%',
+  margin: 'auto'
+}
+
 class App extends React.Component {
+  constructor (){
+    super()
+    this.state = {
+      servers: [
+        {id: 1, name: "APC-WGGenerator1"},
+        {id: 2, name: "APC-WGGenerator2"},
+        {id: 3, name: "APC-WGGenerator3"},
+        {id: 4, name: "APC-WGGenerator4"},
+        {id: 5, name: "APC-WGGenerator5"},
+        {id: 6, name: "APC-WGGenerator6"},
+        {id: 7, name: "APC-WGGenerator7"},
+        {id: 8, name: "APC-WGGenerator8"}
+      ]
+    }
+  }
+
   render() {
-    return (<h1>This is React!</h1>)
+    return (
+      <MuiThemeProvider>
+        <div>
+          <AppBar title="Trigger" />
+          <div className="searchable-list" style={styles}>
+            <p>Generators available:</p>
+            <ServerList servers={this.state.servers}/>
+          </div>
+        </div>
+      </MuiThemeProvider>
+    )
   }
 }
  
---
+++ client/src/ServerList.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Chip from 'material-ui/Chip'
+
+class ServerList extends React.Component {
+  constructor (){
+    super()
+    this.styles = {
+      chip: {
+        margin: '4'
+      },
+      wrapper: {
+        display: 'flex',
+        flexWrap: 'wrap',
+      }
+    }
+  }
+
+  toHtmlTag(server) {
+    return(
+      <Chip style={this.styles.chip}>{server.name}</Chip>
+    )
+  }
+
+  render() {
+    return(
+      <div style={this.styles.wrapper} >
+        {this.props.servers.map((s) => this.toHtmlTag(s))}
+      </div>
+    )
+  }
+}
+
+export default ServerList
