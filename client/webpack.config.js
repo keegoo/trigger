d7d92d964653c15de3c26d2bde50bd42c732d842
CY <onions.yang@gmail.com>
2017-03-05 13:18:38 +0800

    initialize rails api + mongoid + react + webpack + es6

---
+++ client/webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var webpack = require('webpack');
+ 
+module.exports = {
+  module: {
+    loaders: [
+      {
+        loader: "babel-loader",
+        // Skip any files outside of your project's `src` directory
+        include: [
+          path.resolve(__dirname, "src"),
+        ],
+        // Only run `.js` and `.jsx` files through Babel
+        test: /\.jsx?$/,
+        // Options to configure babel with
+        query: {
+          presets: ['es2015', 'react'],
+        }
+      }
+    ]
+  },
+  output: {
+    filename: 'bundle.js'
+  },
+  entry: [
+    './src/App.js'
+  ]
+};
