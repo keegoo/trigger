56377a6bb7b5213cc55d0886abb81806138aae3e
CY <onions.yang@gmail.com>
2017-05-10 14:19:45 +0800

    fix routes mapping issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   
-  resources :generators, only: [:index]
-
-  resources :schedulers, only: [:show, :index, :create, :destroy]
   get 'schedulers/active',        to: 'schedulers#active'
   get 'generators/update_status', to: 'generators#update_status'
+  
+  resources :generators, only: [:index]
+  resources :schedulers, only: [:show, :index, :create, :destroy]
 end
