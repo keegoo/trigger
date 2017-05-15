2e73880bcbbd905b19aa0abd0f17a79a2f3ee3c2
CY <onions.yang@gmail.com>
2017-05-15 16:33:17 +0800

    stylish Heading component

--- client/src/components/heading/Heading.jsx
+++ client/src/components/heading/Heading.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
+import { cyan500, grey300 } from 'material-ui/styles/colors'
 
 const styles = {
   layout: {
-    margin: '10px'
+    margin: '10px 10px 10px 0'
   },
 
   title: {
     fontSize: '24px',
-    marginRight: '10px'
+    marginRight: '10px',
+    color: cyan500
   },
 
   sub: {
-    fontSize: '16px'
+    fontSize: '16px',
+    background: grey300,
+    borderRadius: '2px'
   }
 }
 
@@ -34,7 +38,7 @@ class Heading extends React.Component {
   render() {
     return(
       <div style={styles.layout} >
-        <span style={styles.title}>Schedule: {this.props.title}</span>
+        <span style={styles.title}>Schedule {this.props.title}</span>
         <span style={styles.sub}>{this.statusToString(this.props.status)}</span>
       </div>
     )
